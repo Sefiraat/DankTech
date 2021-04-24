a8b0024daadfce1088fa72042d6076ad491e5dcf
BuildTools <unconfigured@null.spigotmc.org>
2021-04-24 17:37:51 +0100

    Initial

---
+++ src/main/java/io/github/sefiraat/danktech/DankTech.java
@@ -0,0 +1,25 @@
+package io.github.sefiraat.danktech;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DankTech extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+        getLogger().info("########################################");
+        getLogger().info("");
+        getLogger().info("                Dank Tech               ");
+        getLogger().info("            Version 1.0.0.R1            ");
+        getLogger().info("");
+        getLogger().info("          Created by Sefiraat           ");
+        getLogger().info("");
+        getLogger().info("########################################");
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
